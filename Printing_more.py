acb70d9639432472d433f27ce656abf88b2d0a0e
Darkroomdeveloping <47145232+darkroomdeveloping@users.noreply.github.com>
2020-08-21 10:12:19 +0200

    Add files via upload

---
+++ Printing_more.py
@@ -0,0 +1,33 @@
+
+
+"""Example from "Learn Python the hard way, page 32, edited for Python3"""
+
+print ("Mary had a little lamb.")
+print ("Its fleece was white as %s." % 'snow')
+print ("And everywhere that Mary went.")
+print ("." * 40) # what'd that do?
+ddd1 = "C"
+end2 = "h"
+end3 = "e"
+end4 = "e"
+vvv5 = "s"
+end6 = "e"
+end7 = "B"
+end8 = "u"
+end9 = "r"
+xxx10 = "g"
+end11 = "e"
+maryhadalittleboyExtendMixVJRobsonSantosOhSnap = "r"
+
+
+
+# watch that comma at the end. try removing it to see what happens
+print (ddd1 + end2 + end3 + end4 + vvv5 + end6)
+print (end7 + end8 + end9 + xxx10 + end11 + maryhadalittleboyExtendMixVJRobsonSantosOhSnap)
+
+print ("." * 40)
+print("""         Cheese          """)
+print("""              burger          """)
+print("""                    picnic          """)
+
+
